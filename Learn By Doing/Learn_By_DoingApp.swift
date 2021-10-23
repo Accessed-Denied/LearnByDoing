e600404ce0148fbf57362da76d56c05a7b2b89fd
sonusainier <rohitsaini@lambdatest.com>
2021-10-23 23:13:34 +0530

    Initial Commit

---
+++ Learn By Doing/Learn_By_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_By_DoingApp.swift
+//  Learn By Doing
+//
+//  Created by Rohit Saini on 23/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Learn_By_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
